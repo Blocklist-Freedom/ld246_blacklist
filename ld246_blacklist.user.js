125290a0857be0cc29bd23ebf8e6dd050e7a65fc
zxkmm <zxkmm@hotmail.com>
2024-08-08 11:47:03 +0800

    init

---
+++ ld246_blacklist.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         屏蔽指定用户帖子
+// @namespace    Violentmonkey Scripts
+// @version      0.1
+// @description
+// @author       zxkmm
+// @match        https://ld246.com/*
+// @grant        none
+// ==/UserScript==
+
+/*notes
+ * 头像区块 style class：article-list__user fn__flex fn__flex-center
+ * 帖子作者div style class：avatar-small tooltipped__user
+ * 帖子协作人 style class：avatar-small tooltipped__user article-list__participant      （！！！！！query不article-list__participant
+ *
+ * */
+
+(function () {
+  "use strict";
+
+  const blockedUsers = ["science", "zxkmm"];
+
+  let attempts = 0;
+  const intervalId = setInterval(() => {
+    const posts = document.querySelectorAll(".article-list__item");
+    posts.forEach((post) => {
+      const authorName = post
+        .querySelector(".article-list__user .tooltipped__user")
+        .getAttribute("aria-name"); //fetch username
+
+      if (blockedUsers.includes(authorName)) {
+        //main worker
+        // remind way
+        // post.style.display = "none"; // hid
+        post.style.backgroundColor = "red"; // blk
+        // remind way over
+      }
+    });
+
+    attempts++;
+    if (attempts >= 10) {
+      //TODO not hard coded
+      clearInterval(intervalId);
+    }
+  }, 1000);
+})();
