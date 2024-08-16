b00c341f2a78cba4d411cc8fd1d63139b336cbec
sommermorgentraum <24917424+zxkmm@users.noreply.github.com>
2024-08-16 14:56:43 +0800

    bump ver

--- ld246_blacklist.user.js
+++ ld246_blacklist.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         屏蔽链滴用户
 // @namespace    Violentmonkey Scripts
-// @version      0.8
+// @version      0.9
 // @description  屏蔽指定链滴用户的帖子
 // @author       zxkmm
 // @author       frostime
