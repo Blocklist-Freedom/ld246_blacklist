93c359320d2f5d60fea796d3be9e2f9445dfa367
zxkmm <24917424+zxkmm@users.noreply.github.com>
2024-08-20 10:51:06 +0800

    bump version to triugger greasyfork to add @frostime 's new changes

--- ld246_blacklist.user.js
+++ ld246_blacklist.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         屏蔽链滴用户
 // @namespace    Violentmonkey Scripts
-// @version      0.10
+// @version      0.1.1
 // @description  屏蔽指定链滴用户的帖子
 // @author       zxkmm
 // @author       frostime
+// @author       TCOTC
 // @homepage     https://github.com/zxkmm/ld246_blacklist
 // @supportURL   https://github.com/zxkmm/ld246_blacklist/issues
 // @match        https://ld246.com/*
